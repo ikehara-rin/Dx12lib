3fe1c88ef7ba8145243b208fb8a2e7d6d670b1cd
ikehara-rin <120348343+ikehara-rin@users.noreply.github.com>
2025-04-28 16:41:45 +0900

    DirectX12 初期化
    ウィンドウが出る
    DirectX12のデバイスとスワップチェインが作成される

--- DirectX_FrameWork/main.cpp
+++ DirectX_FrameWork/main.cpp
@@ -1,15 +1,27 @@
 #include <windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+using Microsoft::WRL::ComPtr;
 
 // グローバル変数
 HWND g_hWnd = nullptr;
+ComPtr<ID3D12Device> g_device;
+ComPtr<ID3D12CommandQueue> g_commandQueue;
+ComPtr<IDXGISwapChain3> g_swapChain;
 
-// ウィンドウプロシージャ（イベント処理）
+// ウィンドウプロシージャ
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
     case WM_DESTROY:
-        PostQuitMessage(0); // 終了メッセージ
+        PostQuitMessage(0);
         break;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
@@ -17,10 +29,68 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+// DirectX12初期化処理
+void InitD3D12()
+{
+    // デバイス作成
+    {
+        ComPtr<IDXGIFactory6> factory;
+        CreateDXGIFactory1(IID_PPV_ARGS(&factory));
+
+        ComPtr<IDXGIAdapter1> adapter;
+        factory->EnumAdapters1(0, &adapter);
+
+        HRESULT hr = D3D12CreateDevice(
+            adapter.Get(),
+            D3D_FEATURE_LEVEL_11_0, // 最低限動かすための機能レベル
+            IID_PPV_ARGS(&g_device)
+        );
+        assert(SUCCEEDED(hr)); // 作成失敗したら即止める
+    }
+
+    // コマンドキュー作成
+    {
+        D3D12_COMMAND_QUEUE_DESC desc = {};
+        desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+        desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT; // 基本的な命令を流すタイプ
+
+        HRESULT hr = g_device->CreateCommandQueue(&desc, IID_PPV_ARGS(&g_commandQueue));
+        assert(SUCCEEDED(hr));
+    }
+
+    // スワップチェイン作成
+    {
+        ComPtr<IDXGIFactory6> factory;
+        CreateDXGIFactory1(IID_PPV_ARGS(&factory));
+
+        DXGI_SWAP_CHAIN_DESC1 scDesc = {};
+        scDesc.BufferCount = 2; // ダブルバッファ
+        scDesc.Width = 1280;
+        scDesc.Height = 720;
+        scDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // 32bitカラーフォーマット
+        scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        scDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+        scDesc.SampleDesc.Count = 1;
+
+        ComPtr<IDXGISwapChain1> swapChain1;
+        HRESULT hr = factory->CreateSwapChainForHwnd(
+            g_commandQueue.Get(),
+            g_hWnd,
+            &scDesc,
+            nullptr,
+            nullptr,
+            &swapChain1
+        );
+        assert(SUCCEEDED(hr));
+
+        swapChain1.As(&g_swapChain); // バージョンアップ
+    }
+}
+
 // エントリーポイント
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-    // ウィンドウクラスの登録
+    // ウィンドウクラス登録
     WNDCLASSEX wc = {};
     wc.cbSize = sizeof(WNDCLASSEX);
     wc.style = CS_HREDRAW | CS_VREDRAW;
@@ -47,6 +117,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
     ShowWindow(g_hWnd, nCmdShow);
     UpdateWindow(g_hWnd);
 
+    // DirectX12初期化
+    InitD3D12();
+
     // メッセージループ
     MSG msg = {};
     while (msg.message != WM_QUIT)
@@ -58,7 +131,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
         }
         else
         {
-            // --- ここにDirectX12のレンダリング処理を後で入れる ---
+            // ここでレンダリング処理を後で書く
         }
     }
 
